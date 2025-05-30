9dc0cc603b0ca7a12543c85c47b6b39e7592e83a
Regina <lisin003@umn.edu>
2025-05-29 23:46:05 -0500

    fix index/app

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import QuestionForm from "./components/QuestionForm"
 import AnswerForm from "./components/AnswerForm";
-import ResponseList from "./components/ResponseList";
-import NewsletterView from "./components/NewsletterView";
-import "./styles.css";
+//import ResponseList from "./components/ResponseList";
+//import NewsletterView from "./components/NewsletterView";
+//import "./styles.css";
 
 function App() {
   return (
